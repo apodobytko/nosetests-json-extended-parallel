93b60066a7828191d317ae74d86681a98217fdb0
apodobytko <anton.podobytko@upsilonit.com>
2017-02-09 18:53:15 +0300

    Update plugin.py

--- nosetests_json_extended_parallel/plugin.py
+++ nosetests_json_extended_parallel/plugin.py
@@ -1,11 +1,11 @@
-
-from __future__ import absolute_import
-
-import traceback
-from collections import namedtuple
-from nose.plugins import Plugin
+from datetime import datetime
 from multiprocessing import Manager
+from time import time
+import traceback
 
+from nose.exc import SkipTest
+from nose.plugins import Plugin
+from nose.plugins.xunit import id_split, nice_classname, exc_message
 from nosetests_json_extended_parallel.sink import Sink
 
 
@@ -32,99 +32,84 @@ class JsonExtendedPlugin(Plugin):
         self._sink = Sink()
         if not hasattr(self.config, '_nose_json_extended_state_'):
             manager = Manager()
-            self._sink.records = manager.list()
-            self.config._nose_json_extended_state_ = self._sink.records
+            self._sink.results = manager.list()
+            self._sink.stats = manager.dict(**self._sink.stats)
+            self.config._nose_json_extended_state_ = self._sink.results, self._sink.stats
         else:
-            self._sink.records = self.config._nose_json_extended_state_
+            self._sink.results, self._sink.stats = self.config._nose_json_extended_state_
         self._sink.output_file=options.json_file
+
+    def startTest(self, test):
+        self._timer = time()
+
+    def _get_time_taken(self):
+        if hasattr(self, '_timer'):
+            taken = time() - self._timer
+        else:
+            taken = 0.0
+        return taken
 
     def report(self, stream):
         self._sink.write()
 
     def addError(self, test, err, capt=None):
-        if issubclass(err[0], SyntaxError):
-            self._sink.add_syntaxerror(syntax_error_report(err[1]))
+        taken = self._get_time_taken()
+
+        if issubclass(err[0], SkipTest):
+            status = 'skipped'
+            self._sink.stats['skipped'] += 1
         else:
-            self._sink.add('error',
-                           testcase_description(test),
-                           error_report(err))
+            status = 'error'
+            self._sink.stats['errors'] += 1
+        tb = ''.join(traceback.format_exception(*err))
+        test_id = test.id()
+        name = id_split(test_id)[-1]
+        started = getattr(self, '_timer', time())
+        ended = started + taken
+        self._sink.results.append({
+            'id': test_id,
+            'name': name,
+            'time': taken,
+            'status': status,
+            'error_type': nice_classname(err[0]),
+            'started': datetime.fromtimestamp(started).strftime('%x %X'),
+            'ended': datetime.fromtimestamp(ended).strftime('%x %X'),
+            'message': exc_message(err),
+            'tb': tb,
+        })
 
     def addFailure(self, test, err, capt=None, tb_info=None):
-        self._sink.add('failed',
-                       testcase_description(test),
-                       error_report(err))
+        taken = self._get_time_taken()
+        tb = ''.join(traceback.format_exception(*err))
+        self._sink.stats['failures'] += 1
+        test_id = test.id()
+        name = id_split(test_id)[-1]
+        started = getattr(self, '_timer', time())
+        ended = started + taken
+        self._sink.results.append({
+            'id': test_id,
+            'name': name,
+            'time': taken,
+            'status': 'failure',
+            'error_type': nice_classname(err[0]),
+            'started': datetime.fromtimestamp(started).strftime('%x %X'),
+            'ended': datetime.fromtimestamp(ended).strftime('%x %X'),
+            'message': exc_message(err),
+            'tb': tb,
+        })
 
     def addSuccess(self, test, capt=None):
-        self._sink.add('success', testcase_description(test), None)
-
-
-TestCaseDescription = namedtuple('TestCaseDescription',
-                                 'module name filename linenr')
-
-
-def testcase_description(test):
-
-    module, name = _split_id(test.id())
-
-    filename = None
-    linenr = None
-
-    try:
-        filename = test.address()[0]
-    except:
-        pass
-
-    try:
-        methodname = test.test._testMethodName
-        func = getattr(test.test, methodname)
-        linenr = func.__func__.__code__.co_firstlineno
-    except:
-        pass
-
-    return TestCaseDescription(module, name, filename, linenr)
-
-
-ErrorReport = namedtuple('ErrorReport', 'message traceback')
-
-
-def error_report(err):
-    message_list = traceback.format_exception_only(err[0], err[1])
-    message = '\n'.join(message_list).strip('\n')
-
-    tb = list(wrap_traceback(traceback.extract_tb(err[2])))
-
-    return ErrorReport(message, tb)
-
-
-def wrap_traceback(traceback_in):
-    names = ('filename', 'linenr', 'function', 'line')
-    for tb in traceback_in:
-        yield dict(zip(names, tb))
-
-
-def _split_id(test_id):
-    parts = test_id.split('.')
-    return '.'.join(parts[:-1]), parts[-1]
-
-
-
-
-SyntaxErrorReport = namedtuple('SyntaxErrorReport', 'filename linenr column message')
-
-def syntax_error_report(error):
-    filename = error.filename
-    linenr = error.lineno
-    column = error.offset
-
-    message = error.__class__.__name__ + ': ' + str(error)
-
-    if error.text:
-        message += '\n\n' + error.text.strip()
-
-        if error.offset:
-            text = error.text.rstrip()
-            nr_white_space = len(text) - len(text.lstrip())
-            nr = error.offset-nr_white_space-1
-            message += '\n' + ' ' * nr + '^'
-
-    return SyntaxErrorReport(filename, linenr, column, message)
+        taken = self._get_time_taken()
+        self._sink.stats['passes'] += 1
+        test_id = test.id()
+        name = id_split(test_id)[-1]
+        started = getattr(self, '_timer', time())
+        ended = started + taken
+        self._sink.results.append({
+            'id': test_id,
+            'name': name,
+            'time': taken,
+            'status': 'success',
+            'started': datetime.fromtimestamp(started).strftime('%x %X'),
+            'ended': datetime.fromtimestamp(ended).strftime('%x %X'),
+        })
